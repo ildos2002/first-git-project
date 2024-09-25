bf1c83fd42e4d25fb731ba866823e22a7ddb9bbc
Rozentul Ilya <103072320+ildos2002@users.noreply.github.com>
2024-09-25 23:58:55 +0300

    Change FirstGit sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -1,6 +1,6 @@
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("First commit!");
+        System.out.println("GIT DEVELOP!");
         System.out.println("learning!");
         System.out.println("Its LINE!");
         System.out.println("new line.");
