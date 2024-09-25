8d04e2435d44abb2819cd85aaaf82c9b14ecd38b
ildos2002 <103072320+ildos2002@users.noreply.github.com>
2024-09-25 22:23:18 +0300

    Update FirstGit.java
    Add sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,5 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("First commit!");
         System.out.println("learning!");
+        System.out.println("new line.");
     }
 }
